8e1a1025a8dafd3f9de591aa0af20315577a321d
Daria Kolesnik <dariakol91@gmail.com>
2022-02-16 20:26:34 +0100

    function new task added

--- todo list/script.js
+++ todo list/script.js
@@ -1,17 +1,24 @@
 
 //TODO lista
 
+
+const STATUS_TODO = 'TODO';
+const STATUS_INPROGRESS = 'INPROGRESS';
+const STATUS_COMPLETED = 'COMPLETED';
+
+
+
 const exampleTodo = {
     title: 'Zrób trening',
     description: 'Cardio i siłowy',
-    status: 'todo',
+    status: STATUS_TODO,
     autor: 'Daria Koleśnik'
 }
 
 const exampleTodo2 = {
     title: 'Zrób pracę domową',
     description: 'Praca ma być zrobiona na 12 punktów',
-    status: 'inprogress',
+    status: STATUS_INPROGRESS,
     autor: 'Daria Koleśnik'
 }
 
@@ -19,7 +26,7 @@ const exampleTodo2 = {
 const exampleTodo3 = {
     title: 'Namaluj obraz',
     description: 'Użyj farb akrylowych',
-    status: 'inprogress',
+    status: STATUS_INPROGRESS,
     autor: 'Daria Koleśnik'
 }
 
@@ -46,6 +53,17 @@ function displayStatusTodo() {
 }
 
 function display(status) {
+
+    const statusCondition =
+        status === STATUS_INPROGRESS || 
+        status === STATUS_TODO ||
+        status === STATUS_COMPLETED ||
+        status === undefined; 
+
+    if (!statusCondition) {
+        return console.log('Niepoprawny status');
+    }
+
     const todosToDisplay = [];
     for (var i = 0; i < todos.length; i++) {
         if (todos[i].status === status || status === undefined) {
@@ -54,13 +72,37 @@ function display(status) {
     }
 
 
-if(todosToDisplay.length) { 
-    for (var i = 0; i < todosToDisplay.length; i++) {    
-        console.log(`Zadanie ${i + 1}: ${todosToDisplay[i].title} (${todosToDisplay[i].description}) - status: ${todosToDisplay[i].status}`)
+    if (todosToDisplay.length) { 
+        for (var i = 0; i < todosToDisplay.length; i++) { 
+
+            const { title, description, status } = todosToDisplay[i];
+
+            console.log(`Zadanie ${i + 1}: ${title} (${description}) - status: ${status}`);
+        }
+    } else {
+        console.log('brak zadań');
+    }
+}
+
+
+// ZAD 2 
+
+
+function addTodo(title, description, status, autor){
+    const newTodo = {
+        title,
+        description,
+        status,
+        autor, 
+
     }
 
-} else {
-    console.log('brak zadań');
+    todos.push(newTodo);
+    console.log(`Nowe zadanie : ${title} (${description}) - status: ${status}`);
+    console.log('-----');
+    console.log('Wszystkie zadania');
+    display();
 }
 
-}
+// ZAD 3
+
